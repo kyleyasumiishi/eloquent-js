b19e9862bc7c4d0aeca4e0e0ff4b9e3195ff4c64
Kyle <k.yasumiishi@gmail.com>
2018-06-30 15:31:00 -0700

    Added functions problems and started working through data-structures chapter

---
+++ functions/minimum.js
@@ -0,0 +1,8 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(a, b) {
+  return a > b ? b : a;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
