1994dfd97d1061b1f1a2683023842e4bb05be6bc
Kyle <k.yasumiishi@gmail.com>
2018-07-01 15:06:27 -0700

    Passed sum-range

---
+++ data-structures/sum-range.js
@@ -0,0 +1,21 @@
+function range(start, end, step = 1) {
+  const range = [];
+  if (start < end) {
+    for (let i = start; i <= end; i += step) {
+      range.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i += step) {
+      range.push(i);
+    }
+  }
+  return range;
+}
+
+function sum(arr) {
+  let sum = 0;
+  for (let val of arr) {
+    sum += val;
+  }
+  return sum;
+}
