534c4011391ca1b8bb99dbf7e497e69a2e1c5e6e
Kyle <k.yasumiishi@gmail.com>
2020-06-17 19:49:04 -0700

    group

---
+++ secret-life-objects/groups.js
@@ -0,0 +1,39 @@
+/*
+Create a set data structure called "Group".
+*/
+
+class Group {
+    constructor() {
+        this.group = [];
+    }
+    has(value) {
+        return this.group.indexOf(value) > -1;
+    }
+    add(value) {
+        if (!this.has(value)) {
+            this.group.push(value);
+        }
+    }
+    delete(value) {
+        if (this.has(value)) {
+            this.group.splice(this.group.indexOf(value), 1);
+        }
+    }
+    static from(value) {
+        let newGroup = new Group();
+        for (let i = 0; i < value.length; i++) {
+            if (!newGroup.has(value[i])) {
+                newGroup.add(value[i]);
+            }
+        }
+        return newGroup;
+    }
+}
+
+// Tests
+let group = Group.from([10, 20]);
+console.log(group.has(10)); // true
+console.log(group.has(30)); // false
+group.add(10);
+group.delete(10);
+console.log(group.has(10)); // false
